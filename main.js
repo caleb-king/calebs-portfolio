209143c8d49b9a0390e841742c5e9547c4910f53
Caleb King <caleb.king536@gmail.com>
2020-02-25 10:35:28 -0700

    added fonts and other minor stylistic touches

---
+++ main.js
@@ -0,0 +1,27 @@
+function flash(id) {
+  var contact = document.getElementById(id);
+  contact.classList.toggle('purpleFlashOn');
+  setTimeout(() => {
+    contact.classList.toggle('purpleFlashOn');
+  }, 750);
+}
+
+function contactFlash() {
+  flash('contact');
+}
+
+function projectsFlash() {
+  flash('projects');
+}
+
+function bioFlash() {
+  flash('bio');
+}
+
+function setup() {
+  document.getElementById('contact-nav').onclick = contactFlash;
+  document.getElementById('projects-nav').onclick = projectsFlash;
+  document.getElementById('bio-nav').onclick = bioFlash;
+}
+
+window.onload = setup;
